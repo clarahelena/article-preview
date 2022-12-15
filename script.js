ee6b6fa50f9d69a1a3ea2ea5d7892631ea69c20d
Clara Helena <clara.helena.souza@gmail.com>
2022-12-14 22:36:28 -0300

    javascript do botao share

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const botao = document.querySelector('.div-irma');
+const botao1 = document.querySelector('.div-irma');
+const botao2 = document.querySelector('.div-irma-mobile');
 const secao = document.querySelector('.share');
-console.log(secao);
-function evento() {
-    secao.classList.toggle('hoa');
+const minimals = document.querySelector('.minimals');
+const minimalsMobile = document.querySelector('.minimals-mobile');
+console.log(botao2);
+
+
+function share() {
+    if (window.innerWidth >= 900) {
+        secao.classList.toggle('hoa');
+    } else {
+        secao.classList.remove('hoa');
+         minimals.style.display = "none";
+         minimalsMobile.style.display = "flex";
+    }
 }
-botao.addEventListener('click', evento);
+
+function shareMobile() {
+    minimals.style.display = "flex";
+    minimalsMobile.style.display = "none";
+}
+
+botao1.addEventListener('click', share);
+botao2.addEventListener('click', shareMobile);
+
